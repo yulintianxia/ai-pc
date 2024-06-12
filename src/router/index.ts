03bfe0cd2da0a63aabb1e711a327f807f7a35c2d
raoyiyu <793284205@qq.com>
2024-06-12 17:40:25 +0800

    feat; 修改了路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -112,7 +112,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/login.vue"),
+      component: () => import("../views/Login.vue"),
       meta: {
         auth: false,
       },
